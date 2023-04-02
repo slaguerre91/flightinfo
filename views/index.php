bbf97118196d08fc02d37215f9c0db3ea663a1f6
Samyr Laguerre <samyrmlaguerre@gmail.com>
2023-04-01 23:27:18 -0400

    favicon fix

--- views/index.php
+++ views/index.php
@@ -16,7 +16,7 @@ require_once('../controllers/review/recent.php');
     <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
-    <link rel="manifest" href="../assets/views/site.webmanifest">
+    <link rel="manifest" href="assets/views/site.webmanifest">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
